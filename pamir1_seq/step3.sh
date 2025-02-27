ef703f1dde8f2b3fc7c9aaab8aa43e7a9b5acd6d
Luke Horgan <soystuff@gmail.com>
2025-02-27 10:07:30 -0500

    add timings

--- pamir1_seq/step3.sh
+++ pamir1_seq/step3.sh
@@ -1,15 +1,15 @@
 DATA_PATH=$1
 SCENE=$2
 
-colmap feature_extractor \
+time colmap feature_extractor \
     --database_path $DATA_PATH/$SCENE/output/database.db  \
     --image_path ${DATA_PATH}/${SCENE}/Images
 
 # Alternative for larger datasets: spatial_matcher
-colmap sequential_matcher \
+time colmap sequential_matcher \
     --database_path $DATA_PATH/$SCENE/output/database.db 
 
-colmap pose_prior_mapper \
+time colmap pose_prior_mapper \
     --database_path $DATA_PATH/$SCENE/output/database.db  \
     --image_path ${DATA_PATH}/${SCENE}/Images \
     --output_path $DATA_PATH/$SCENE/output/sparse \
