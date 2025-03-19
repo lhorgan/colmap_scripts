f9cee2a06aad26b512554efbb4a01566e3cd2608
Luke Horgan <soystuff@gmail.com>
2025-03-19 18:46:08 -0400

    switch to sequential matcher

--- clean/shell_scripts/features_and_matching.sh
+++ clean/shell_scripts/features_and_matching.sh
@@ -5,7 +5,7 @@ time colmap feature_extractor \
     --database_path $DATA_PATH/$SCENE/output/database.db  \
     --image_path ${DATA_PATH}/${SCENE}/Images
 
-time colmap exhaustive_matcher \
-    --database_path $DATA_PATH/$SCENE/output/database.db
-# time colmap sequential_matcher \
-#     --database_path $DATA_PATH/$SCENE/output/database.db 
+# time colmap exhaustive_matcher \
+#     --database_path $DATA_PATH/$SCENE/output/database.db
+time colmap sequential_matcher \
+    --database_path $DATA_PATH/$SCENE/output/database.db 
