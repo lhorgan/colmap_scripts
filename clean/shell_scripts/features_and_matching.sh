ddd9afca077561ac2ee81b38a4782cf86686b278
Luke Horgan <soystuff@gmail.com>
2025-03-20 09:50:36 -0400

    run

--- clean/shell_scripts/features_and_matching.sh
+++ clean/shell_scripts/features_and_matching.sh
@@ -3,7 +3,8 @@ SCENE=$2
 
 time colmap feature_extractor \
     --database_path $DATA_PATH/$SCENE/output/database.db  \
-    --image_path ${DATA_PATH}/${SCENE}/Images
+    --image_path ${DATA_PATH}/${SCENE}/Images \
+    --ImageReader.single_camera_per_folder 1 # Necessary?
 
 # time colmap exhaustive_matcher \
 #     --database_path $DATA_PATH/$SCENE/output/database.db
