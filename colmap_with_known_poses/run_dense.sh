6e00ec46a84ddd335361d9a1fd42744bbd2f0ec4
Luke Horgan <soystuff@gmail.com>
2025-05-01 17:42:37 -0400

    run dense

--- colmap_with_known_poses/run_dense.sh
+++ colmap_with_known_poses/run_dense.sh
@@ -1,32 +1,32 @@
 DATA_PATH=/home/luke/Documents/hell/apr29
-SCENE=Right100_2
+SCENE=Right100
 
 echo "Running image undistorter"
 
 colmap image_undistorter \
     --image_path $DATA_PATH/$SCENE/Images \
-    --input_path $DATA_PATH/$SCENE/output/sparse/0 \
-    --output_path $DATA_PATH/$SCENE/output/dense \
+    --input_path $DATA_PATH/$SCENE/sparse \
+    --output_path $DATA_PATH/$SCENE/dense \
     --output_type COLMAP \
     --max_image_size 960
 
 echo "Running patch match stereo"
 
 colmap patch_match_stereo \
-    --workspace_path $DATA_PATH/$SCENE/output/dense \
+    --workspace_path $DATA_PATH/$SCENE/dense \
     --workspace_format COLMAP \
     --PatchMatchStereo.geom_consistency true
 
 echo "Running stereo fusion"
 
 colmap stereo_fusion \
-    --workspace_path $DATA_PATH/$SCENE/output/dense \
+    --workspace_path $DATA_PATH/$SCENE/dense \
     --workspace_format COLMAP \
     --input_type geometric \
-    --output_path $DATA_PATH/$SCENE/output/dense/fused.ply
+    --output_path $DATA_PATH/$SCENE/dense/fused.ply
 
 echo "Running poisson mesher"
 
 colmap poisson_mesher \
-    --input_path $DATA_PATH/$SCENE/output/dense/fused.ply \
-    --output_path $DATA_PATH/$SCENE/output/dense/meshed-poisson.ply
+    --input_path $DATA_PATH/$SCENE/dense/fused.ply \
+    --output_path $DATA_PATH/$SCENE/dense/meshed-poisson.ply
